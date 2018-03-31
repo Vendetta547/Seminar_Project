b8c9de9a676d91ddb7d04b063688b098fc6c394e
dmartin <dmartin@atomicorp.com>
2018-03-31 01:03:55 -0400

    adds button code that sends predefined message to bluetooth app. One button is supported at the moment. Circuit schematic coming later.

--- Arduino_Progs/Feather_UART_Comm/bluetooth_glove/bluetooth_glove.ino
+++ Arduino_Progs/Feather_UART_Comm/bluetooth_glove/bluetooth_glove.ino
@@ -1,32 +1,3 @@
-/* button code example */
-/*  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-    buttonState = digitalRead(buttonPin);
-    if (buttonState != lastButtonState) {
-      if (buttonState == HIGH) {
-        //
-      } else {
-        //Serial.println("LOW");
-        ble.print("AT+BleKeyboard=");
-        ble.println("Test message from button");
-      }
-      delay(50);
-    }
-    lastButtonState = buttonState;
-
-/*********************************************************************
- This is an example for our nRF51822 based Bluefruit LE modules
-
- Pick one up today in the adafruit shop!
-
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
-
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
-
 #include <Arduino.h>
 #include <SPI.h>
 #include "Adafruit_BLE.h"
@@ -39,40 +10,9 @@
   #include <SoftwareSerial.h>
 #endif
 
-/*=========================================================================
-    APPLICATION SETTINGS
-
-    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
-   
-                              Enabling this will put your Bluefruit LE module
-                              in a 'known good' state and clear any config
-                              data set in previous sketches or projects, so
-                              running this at least once is a good idea.
-   
-                              When deploying your project, however, you will
-                              want to disable factory reset by setting this
-                              value to 0.  If you are making changes to your
-                              Bluefruit LE device via AT commands, and those
-                              changes aren't persisting across resets, this
-                              is the reason why.  Factory reset will erase
-                              the non-volatile memory where config data is
-                              stored, setting it back to factory default
-                              values.
-       
-                              Some sketches that require you to bond to a
-                              central device (HID mouse, keyboard, etc.)
-                              won't work at all with this feature enabled
-                              since the factory reset will clear all of the
-                              bonding data stored on the chip, meaning the
-                              central device won't be able to reconnect.
-    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
-    MODE_LED_BEHAVIOUR        LED activity, valid options are
-                              "DISABLE" or "MODE" or "BLEUART" or
-                              "HWUART"  or "SPI"  or "MANUAL"
-    -----------------------------------------------------------------------*/
-    #define FACTORYRESET_ENABLE         1
-    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-    #define MODE_LED_BEHAVIOUR          "MODE"
+#define FACTORYRESET_ENABLE         1
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
 /*=========================================================================*/
 
 // Create the bluefruit object, either software serial...uncomment these lines
@@ -101,14 +41,12 @@ void error(const __FlashStringHelper*err) {
   while (1);
 }
 
-/**************************************************************************/
-/*!
-    @brief  Sets up the HW an the BLE module (this function is called
-            automatically on startup)
-*/
-/**************************************************************************/
+
+const int play_button = 6;
+
 void setup(void)
 {
+  pinMode(play_button, INPUT);
   while (!Serial);  // required for Flora & Micro
   delay(500);
 
@@ -168,8 +106,27 @@ void setup(void)
     @brief  Constantly poll for new command or response data
 */
 /**************************************************************************/
+int buttonState = 0; 
+int lastButtonState = 0;
 void loop(void)
 {
+// check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    buttonState = digitalRead(play_button);
+    if (buttonState != lastButtonState) {
+      if (buttonState == LOW) {
+        //
+      } else {
+        //Serial.println("LOW");
+        Serial.print("[Send] ");
+        Serial.println("Test message from button");
+
+        ble.print("AT+BLEUARTTX=");
+        ble.println("Test message from button"); 
+      }
+      delay(50);
+    }
+    lastButtonState = buttonState;
+  
   // Check for user input
   char inputs[BUFSIZE+1];
 
