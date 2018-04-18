5466499a44f6a04def81f87330578f57a5df2032
dmartin <dmartin@atomicorp.com>
2018-04-18 06:24:29 -0400

    adds resume app on notification tap

--- Android_Apps/Final_App/app/src/main/java/com/adafruit/bluefruit/le/connect/app/ForegroundService.java
+++ Android_Apps/Final_App/app/src/main/java/com/adafruit/bluefruit/le/connect/app/ForegroundService.java
@@ -1,6 +1,7 @@
 package com.adafruit.bluefruit.le.connect.app;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -25,19 +26,21 @@ public class ForegroundService extends Service {
             Log.i(LOG_TAG, "Received Start Foreground Intent ");
 
 
-            Intent notificationIntent = new Intent(this, MainActivity.class);
-            notificationIntent.setAction(Constants.ACTION.MAIN_ACTION);
-            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
             Bitmap icon = BitmapFactory.decodeResource(getResources(),
                     R.mipmap.green_icon);
+
+
+            Intent notifyIntent = new Intent(this, UartActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notifyIntent, 0);
+
 
             Notification notification = new NotificationCompat.Builder(this)
                     .setContentTitle("DJ Glovie")
                     .setContentText("Connected")
                     .setSmallIcon(R.mipmap.ic_launcher)
-                    .setPriority(Notification.PRIORITY_MIN)
+                    .setContentIntent(pendingIntent)
+                    .setPriority(Notification.PRIORITY_MIN) // only displays when notification bar is swiped down (good idea?)
                     .setLargeIcon(
                             Bitmap.createScaledBitmap(icon, 128, 128, false))
                     .setOngoing(true).build();
