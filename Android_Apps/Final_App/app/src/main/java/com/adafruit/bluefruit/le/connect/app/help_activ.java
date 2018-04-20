8862fb52e1d03f864b830795aca6e95444ea7b60
Spivey <mcs2hw@uvawise.edu>
2018-04-20 19:00:19 -0400

    adds video to help page

--- Android_Apps/Final_App/app/src/main/java/com/adafruit/bluefruit/le/connect/app/help_activ.java
+++ Android_Apps/Final_App/app/src/main/java/com/adafruit/bluefruit/le/connect/app/help_activ.java
@@ -1,17 +1,24 @@
 package com.adafruit.bluefruit.le.connect.app;
 
 import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.adafruit.bluefruit.le.connect.R;
 
 public class help_activ extends AppCompatActivity {
 
-
-
+    Button play_button;
+    VideoView video_window;
+    MediaController mediaC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +28,18 @@ public class help_activ extends AppCompatActivity {
         setTitle("About");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // display page in a webview format
-        WebView infoWebView = (WebView) findViewById(R.id.infoWebView);
-        if (infoWebView != null) {
-            infoWebView.setBackgroundColor(Color.TRANSPARENT);
-            infoWebView.loadUrl("file:///android_asset/help.html");
-        }
+        play_button = (Button)findViewById(R.id.video_play_button);
+        video_window = (VideoView)findViewById(R.id.video_view);
+        mediaC = new MediaController(this);
+    }
+
+    public void videoPlay(View v) {
+        String videopath = "android.resource://com.adafruit.bluefruit.le.connect/"+R.raw.djglovie_flipped;
+        Uri uri = Uri.parse(videopath);
+        video_window.setVideoURI(uri);
+        video_window.setMediaController(mediaC);
+        mediaC.setAnchorView(video_window);
+        video_window.start();
     }
 
 
